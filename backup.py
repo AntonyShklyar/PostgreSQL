69ba56aa2a3e90eb2359eb9a2af322494c967f54
AntonyShklyar <95246873+AntonyShklyar@users.noreply.github.com>
2021-12-24 18:18:20 +0300

    Update backup.py

--- backup.py
+++ backup.py
@@ -34,12 +34,6 @@ if not os.path.getsize('/var/log/debugdb.log')/(1024*1024*1024)==0: os.remove('/
 shutil.copyfile("/var/log/backupdb.log", "/var/log/debugdb.log") 
 #Creating a log file to store the events of one script launch
 os.remove('/var/log/backupdb.log'); open("/var/log/backupdb.log", "w+")
-if set('01').issubset(socket.gethostname()):
-	dir1='/mnt/dbbackup/OCOD/'
-        dir2='/mnt/dbbackup/RCOD/'
-elif set('02').issubset(socket.gethostname()):
-	dir1='/mnt/dbbackup/RCOD/'
-        dir2='/mnt/dbbackup/OCOD/'
 def massive(IP=[]):
 	if set('01').issubset(socket.gethostname()):
         	IPIZ = ['10.111.15.54', '10.111.15.63']
@@ -79,10 +73,11 @@ def backup(var):
 					    with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' PostgreSQL Database Backup Successful'+'\n')
                                         else:
                                             with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' PostgreSQL Database Backup Unsuccessful'+'\n')
+					    return 1
                                         TIME='-' + str(round(((time.time() - start_time)/60)+30)).split('.')[0]                                	
 					if not (statcor==0 and statpace==0):
 						with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' PostgreSQL DB is not running'+'\n')
-                                        #Deleting an incorrectly created backup due to the disabling of corosync and pacemaker services during the creation of the backup
+                                                #Deleting an incorrectly created backup due to the disabling of corosync and pacemaker services during the creation of the backup
                                         	os.remove(path + '/temp/' + 'base.tar.gz')
 						test='OCOD' if set('01').issubset(socket.gethostname()) else 'RCOD'
 						with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' An incorrectly created backup copy of the database has been deleted'+test+'\n')
@@ -251,11 +246,11 @@ def sync(a1, a2):
 						continue
 					elif e==d:
 						exit_code = subprocess.call(['cp', var1+j, var2])
-						if exit_code==0:
-							os.system('echo $(date +"%Y%m%d-%H%M%S")     Synchronization of archive $(echo $j) of the backup copy of the DB PostgreSQL RCOD and OCOD Successfully >> /var/log/backupdb.log')
-						else:
-							os.system('echo $(date +"%Y%m%d-%H%M%S")     Synchronization of archive $(echo $j) of the backup copy of the DB PostgreSQL RCOD and OCOD Unsuccessfully >> /var/log/backupdb.log')
-						break
+						if exit_code==0: 
+						with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Synchronization of archive '+j+' of the backup copy of the DB PostgreSQL RCOD and OCOD Successfully'+'\n')
+					else:
+						with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Synchronization of archive '+j+' of the backup copy of the DB PostgreSQL RCOD and OCOD Unsuccessfully'+'\n')
+					if len(a)==1: break
 def search():
 	ls = subprocess.Popen(("ls", "-I", "wal*", "/mnt/dbbackup/RCOD/"), stdout=subprocess.PIPE); a = subprocess.check_output(('grep', 'tar.gz'), stdin=ls.stdout); ls.wait(); a=len(a.splitlines())
 	ls = subprocess.Popen(("ls", "-I", "wal*", "/mnt/dbbackup/OCOD/"), stdout=subprocess.PIPE); b = subprocess.check_output(('grep', 'tar.gz'), stdin=ls.stdout); ls.wait(); b=len(b.splitlines())
@@ -287,42 +282,71 @@ def mounts(g):
         	n=1
 	return n
 var=0
+m=0
 my_array=massive()
+IP=['s39bd1iz01.ac.com', 's39bd1iz02.ac.com', 's39bd2iz01.ac.com', 's39bd2iz02.ac.com', 's39crsvn01.vp.com', 's39crsvn02.vp.com', 's39crsin01.in.com', 's39ccrsin02.in.com']
+a=socket.gethostname()			     
 for g in my_array:
         var += 1
+	for i in IP:
+		m += 1
+		if i==a:
+			if var==1 and set('01').issubset(socket.gethostname()): path='/dbbackup/OCOD/' + socket.gethostname()
+			elif var==1 and set('02').issubset(socket.gethostname()): path='/dbbackup/RCOD/' + socket.gethostname()
+			elif var==2 and set('01').issubset(socket.gethostname()): path='/dbbackup/RCOD/' + socket.gethostname()
+			elif var==2 and set('02').issubset(socket.gethostname()): path='/dbbackup/OCOD/' + socket.gethostname()	
+		elif m != len(IP):
+			continue
+		else:
+			if var==1 and set('01').issubset(socket.gethostname()): path='/dbbackup/OCOD/'
+			elif var==1 and set('02').issubset(socket.gethostname()): path='/dbbackup/RCOD/'
+			elif var==2 and set('01').issubset(socket.gethostname()): path='/dbbackup/RCOD/'
+			elif var==2 and set('02').issubset(socket.gethostname()): path='/dbbackup/OCOD/'	
         #Checking the availability of hypervisor servers with ICMP backup storages
 	mount = subprocess.Popen(("ping", "-c4", g), stdout=subprocess.PIPE); exit_code = subprocess.check_output(("sed", "-n", '1,/^---/d;s/%.*//;s/.*, //g;p;q'), stdin=mount.stdout); mount.wait()
-        if int(exit_code.replace("\n","")) == 100:
+        test='OCOD' if (set('01').issubset(socket.gethostname()) and var==1) or (set('02').issubset(socket.gethostname()) and var==2) else 'RCOD'
+	if int(exit_code.replace("\n","")) == 100:
                 if var==1:
-                        os.system('echo $(date +"%Y%m%d-%H%M%S")     Server with storage $(if [[ $(hostname | grep 01) ]]; then echo OCOD; elif [[ $(hostname | grep 02) ]]; then echo RCOD; fi) is not available       >> /var/log/backupdb.log')
+			with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Server with storage'+test+'is not available'+'\n')
                         continue
                 else:
-                        os.system('echo $(date +"%Y%m%d-%H%M%S")     Server with storage $(if [[ $(hostname | grep 01) ]]; then echo OCOD; elif [[ $(hostname | grep 02) ]]; then echo RCOD; fi) is not available       >> /var/log/backupdb.log')
+                        with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Server with storage'+test+'is not available'+'\n')
                         break
         else:
-                os.system('echo $(date +"%Y%m%d-%H%M%S")   $( if [[ ($var = 1) && $(hostname | grep 01) ]]; then echo Server with storage of OCOD is available; elif [[ ($var = 1) && $(hostname | grep 02) ]]; then echo Server with storage of RCOD is available; elif [[ ($var = 2) && $(hostname | grep 01) ]]; then echo Server with storage of OCOD is available; elif [[ ($var = 2) && $(hostname | grep 02) ]]; then echo Server with storage of RCOD is available; fi)      >> /var/log/backupdb.log')
-                if var==1:
-                        #Mounting storages on a server with a database, if not mounted
-                        #Checking the ability to write to storages mounted on a server with a database
-                        exit_code = subprocess.call(["touch", dir1+'test'])
+                with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Server with storage'+test+'is available'+'\n')
+                #Mounting storages on a server with a database, if not mounted
+                #Checking the ability to write to storages mounted on a server with a database
+		exit_code = subprocess.call(["touch", path+'test'])
+		bdarch=''
 			if mounts(g)==1:
-                                os.system('mount -a')
-                                if mounts(g)==1:
-                                        os.system('echo $(date +"%Y%m%d-%H%M%S")   Problems with the operation of the smbd service on the server with the repository of $(if [[ $(hostname | grep 01) ]]; then echo OCOD; elif [[ $(hostname | grep 02) ]]; then echo RCOD; fi) >> /var/log/backupdb.log')
+                                b=subprocess.Popen(["mount", "//"+g+"/bkp"+path, s, "-o", "vers=3.0,credentials=/opt/creds/CredIZOCOD.txt,rw,file_mode=0666,dir_mode=0777"]).wait();
+                                if b!=0 and var==1:
+					with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Problems with the operation of the smbd service on the server with the repository of'+test+'\n')
                                         continue
-                                else:
-                                        os.system('echo $(date +"%y%m%d-%h%m%s")   PostgreSQL database backup storage is $(if [[ $(hostname | grep 01) ]]; then echo OCOD; elif [[ $(hostname | grep 02) ]]; then echo RCDO; fi) mounted successfully   >> /var/log/backupdb.log')
+				elif b!=0 and var==2:
+					os.system('echo $(date +"%Y%m%d-%H%M%S")   Problems with the operation of the smbd service on the server with the repository of $(if [[ $(hostname | grep 01) ]]; then echo RCOD; elif [[ $(hostname | grep 02) ]]; then echo OCOD; fi) >> /var/log/backupdb.log')
+                                        exit()
+                                elif b==0 and var==1:
+					with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' PostgreSQL database backup storage is'+test+'mounted successfully'+'\n')
                                         bdarch=backup(1)
-                        elif exit_code != 0:
-                                os.system('echo $(date +"%Y%m%d-%H%M%S")   Problems with the operation of the smbd service on the server with the repository of $(if [[ $(hostname | grep 01) ]]; then echo OCOD; elif [[ $(hostname | grep 02) ]]; then echo RCOD; fi) >> /var/log/backupdb.log')
-                                continue
-                        elif exit_code == 0:
+				
+                        elif exit_code != 0 and var==1:
+                                with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Problems with the operation of the smbd service on the server with the repository of'+test+'\n')
+                                bdarch='1'
+				continue
+			elif exit_code != 0 and var==2:
+                                with open("/var/log/backupdb.log","a+") as stdout: stdout.write(str(datetime.now().strftime("%Y%m%d-%H%M%S"))+' Problems with the operation of the smbd service on the server with the repository of'+test+'\n')
+                                exit()
+                        elif exit_code == 0 and var==1:
                                 bdarch=backup(1)
+			elif exit_code == 0 and var==2 and bdarch=='1':
+				bdarch=backup(1)
+			elif exit_code == 0 and var==2 and bdarch!='1':
+				
 	 	if var==2:
                         #Mounting storages on a server with a database, if not mounted
                         #Checking the ability to write to storages mounted on a server with a database
                         exit_code = subprocess.call(["touch", dir2+'test'])
-                        ls = subprocess.Popen(("mount"), stdout=subprocess.PIPE); mount = subprocess.check_output(("grep", g), stdin=ls.stdout); ls.wait();
 			if mounts(g)==1:
                                 os.system('mount -a')
                                 if mounts(g)==1:
